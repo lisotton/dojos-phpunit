a290e87f3dd6af71e99c3151b3a1311c352b95c3
Leandro L. Isotton <pequenoo@gmail.com>
2018-02-01 20:07:33 -0200

    First class of KataBankOCR

---
+++ src/KataBankOCR/Digit.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Dojo\KataBankOCR;
+
+class Digit {
+
+    private $number;
+
+    function __construct($number) {
+        $this->number = $number;
+    }
+
+    function convertToNumber() {
+        $mapping = $this->getMapping();
+        return isset($mapping[$this->number]) ? $mapping[$this->number] : "?";
+    }
+
+    private function getMapping() {
+        $mapping = [
+            0 => " _ " .
+                 "| |" .
+                 "|_|",
+            1 => "   " .
+                 "  |" .
+                 "  |",
+            2 => " _ " .
+                 " _|" .
+                 "|_ ",
+            3 => " _ " .
+                 " _|" .
+                 " _|",
+            4 => "   " .
+                 "|_|" .
+                 "  |",
+            5 => " _ " .
+                 "|_ " .
+                 " _|",
+            6 => " _ " .
+                 "|_ " .
+                 "|_|",
+            7 => " _ " .
+                 "  |" .
+                 "  |",
+            8 => " _ " .
+                 "|_|" .
+                 "|_|",
+            9 => " _ " .
+                 "|_|" .
+                 " _|",
+        ];
+        return array_flip($mapping);
+
+    }
+
+}
---
+++ tests/KataBankOCR/DigitTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Dojo\KataBankOCR\Digit;
+
+final class DigitTest extends TestCase {
+
+    public function testZero() {
+        $number = " _ " .
+                "| |" .
+                "|_|";
+        $digit = new Digit($number);
+        $this->assertEquals(0, $digit->convertToNumber());
+    }
+
+    public function testOne() {
+        $number = "   " .
+                "  |" .
+                "  |";
+        $digit = new Digit($number);
+        $this->assertEquals(1, $digit->convertToNumber());
+    }
+
+    public function testInvalidNumber() {
+        $number = "_____||||";
+        $digit = new Digit($number);
+        $this->assertEquals("?", $digit->convertToNumber());
+    }
+
+    /**
+     * @dataProvider digitsProvider
+     */
+    public function testConvertToNumber($expected, $representation) {
+        $digit = new Digit($representation);
+        $this->assertEquals($expected, $digit->convertToNumber());
+    }
+
+    public function digitsProvider() {
+        return [
+            [0, " _ | ||_|"],
+            [1, "     |  |"],
+            [2, " _  _||_ "],
+            [3, " _  _| _|"],
+            [4, "   |_|  |"],
+            [5, " _ |_  _|"],
+            [6, " _ |_ |_|"],
+            [7, " _   |  |"],
+            [8, " _ |_||_|"],
+            [9, " _ |_| _|"],
+        ];
+    }
+
+}
